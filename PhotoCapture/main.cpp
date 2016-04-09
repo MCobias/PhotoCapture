320e7e6c3e1c368eeb457649fc2ae24401322f93
MCobias <marcelocobias18@gmail.com>
2016-04-09 13:29:41 -0300

    Initial Commit

---
+++ PhotoCapture/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PhotoCapture
+//
+//  Created by Marcelo Cobias on 4/9/16.
+//  Copyright © 2016 Marcelo Cobias. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
